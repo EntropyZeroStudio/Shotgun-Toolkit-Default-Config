55ffe76bd554780555f07105d135a30f0a9daf67
PedroCabrera <pedro.c.vfx@gmail.com>
2021-01-21 13:23:34 +0100

    Workfiles Edit

--- README.md
+++ README.md
@@ -9,4 +9,7 @@ Welcome to the Shotgun Pipeline Toolkit default configuration!
 For more information, go to the following url:
 https://support.shotgunsoftware.com/hc/en-us/articles/115000067493-Integrations-Admin-Guide
 
+
+asafsafsaf
+
 -------------------------------------------------------------------------
--- hooks/tk-multi-publish2/basic/nuke/get_users.py
+++ hooks/tk-multi-publish2/basic/nuke/get_users.py
@@ -6,5 +6,3 @@ def get_userID():
     userDict = {'bgil': 286, 'daniel': 61, 'david': 58, 'dnicolas': 70, 'dperea' : 152, 'hector': 62, 'jaime': 53, 'jordi': 54, 'jalvarez': 72, 'jgomez': 151, 'lgarcia': 118, 'lucia': 63, 'mduque': 187, 'mmartinez': 319, 'pedro': 51, 'pibanez': 153}
     sg_user = userDict.get(userName)
     return sg_user
-
-print(get_userID())
